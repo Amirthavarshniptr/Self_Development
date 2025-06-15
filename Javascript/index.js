7dc71fadf9f1c1c72b0b41915695b6b9fc5b3360
Amirthavarshniptr <z5453670@ad.unsw.edu.au>
2025-06-15 21:12:14 +1000

    Staton people number adder

---
+++ Javascript/index.js
@@ -0,0 +1,17 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countstr = count + "-"
+    saveEl.textContent += countstr
+    console.log(count)
+    countEl.innerText=0
+    count=0
+}

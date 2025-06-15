30e45d0125ed51c5008d5df6090ab92b040a9713
Amirthavarshniptr <z5453670@ad.unsw.edu.au>
2025-06-16 02:35:29 +1000

    Practice javascript

---
+++ Javascript/Practice/dictionary.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "Amirtha",
+    age: 25,
+    country: "Australia"
+}
+
+function logData(){
+    console.log(person.name + " is " + person.age+ " years old and lives in "+ person.country)
+}
+logData()

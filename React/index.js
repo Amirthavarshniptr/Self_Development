868d00bbe6a890c1cad0dd63c9559b1946d6ac4a
Amirthavarshniptr <z5453670@ad.unsw.edu.au>
2025-06-15 19:28:47 +1000

    Initial commit

---
+++ React/index.js
@@ -0,0 +1,8 @@
+
+errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+function purchase(){
+    console.log("button clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
+}

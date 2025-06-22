54699480d085d0a51980e575f488332bbdafeff0
Amirthavarshniptr <z5453670@ad.unsw.edu.au>
2025-06-22 20:10:18 +1000

    Deleted for env changes

--- React/index.js
+++
@@ -1,8 +0,0 @@
-
-errorParagraph = document.getElementById("error")
-console.log(errorParagraph)
-
-function purchase(){
-    console.log("button clicked")
-    errorParagraph.textContent = "Something went wrong, please try again"
-}
--- React/index.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDom from "react-dom/client"
-
-ReactDom.createRoot(document.getElementById("root")).render(
-    <ul>
-        <li>Super popular JS library</li>
-        <li>Will help me more enjoyable</li>
-        <li>React has a pretty cool logo</li>
-    </ul>
-)
